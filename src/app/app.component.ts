dbe6b3ae009f94d6474dc184a9b7faf572fdc1ef
MariyaHozjan <hozjan.mariya@gmail.com>
2024-01-31 21:20:13 +0100

    Update real estate data and styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from './auth.service';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  constructor(private authService: AuthService, private router: Router) {}
+export class AppComponent implements OnInit {
+  currentRoute: string = '';
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe(() => {
+        let root = this.router.routerState.snapshot.root;
+        while (root.firstChild) {
+          root = root.firstChild;
+        }
+        this.currentRoute = root.routeConfig?.path || '';
+      });
+  }
+
+  getRouteName(route: string): string {
+    const routeNames: { [key: string]: string } = {
+      login: 'Home',
+      listings: 'Listings',
+      edit: 'Edit Listings',
+    };
+
+    return routeNames[route] || 'Home';
+  }
 
   signOut(): void {
     console.log('Logging out');
